81ca388bb5089efba84f6ddc1ec34ef014b79956
Q6int <q6int@yahoo.com>
2019-01-19 15:58:10 +0330

    added cli DownloadMultiConnection  cmd

--- cli/commands_file.go
+++ cli/commands_file.go
@@ -4,8 +4,18 @@ import (
 	"mime"
 	"os"
 	"path"
+	"strconv"
+	"sync"
+	"time"
 
+	"git.ronaksoftware.com/ronak/riversdk/filemanager"
+
+	"go.uber.org/zap"
+
+	"git.ronaksoftware.com/ronak/riversdk/domain"
+	"git.ronaksoftware.com/ronak/riversdk/log"
 	"git.ronaksoftware.com/ronak/riversdk/msg"
+	"git.ronaksoftware.com/ronak/riversdk/repo"
 	ishell "gopkg.in/abiosoft/ishell.v2"
 )
 
@@ -61,8 +71,142 @@ var Download = &ishell.Cmd{
 	},
 }
 
+var DownloadMultiConnection = &ishell.Cmd{
+	Name: "DownloadMultiConnection",
+	Func: func(c *ishell.Context) {
+		messageID := fnGetMessageID(c)
+		segmentCount := 8
+		x := new(msg.MediaDocument)
+		m := repo.Ctx().Messages.GetMessage(messageID)
+		if m.MediaType == msg.MediaTypeDocument {
+
+			err := x.Unmarshal(m.Media)
+			if err != nil {
+				log.LOG_Error("Error", zap.Error(err))
+				return
+			}
+
+		}
+		totalParts := 0
+		count := int(x.Doc.FileSize / domain.FilePayloadSize)
+		if (count * domain.FilePayloadSize) < int(x.Doc.FileSize) {
+			totalParts = count + 1
+		} else {
+			totalParts = count
+		}
+
+		partsQueue := make(chan int, totalParts)
+		// add all parts to queue
+		for i := 0; i < totalParts; i++ {
+			partsQueue <- i
+		}
+		fileBuff := make(map[int][]byte)
+		fileLock := &sync.Mutex{}
+		wg := &sync.WaitGroup{}
+		wg.Add(segmentCount)
+		for i := 0; i < segmentCount; i++ {
+			go downloadWorker(i, wg, partsQueue, fileBuff, fileLock, x)
+		}
+		wg.Wait()
+
+		// save file
+
+		strName := strconv.FormatInt(domain.SequentialUniqueID(), 10) + ".tmp"
+		f, err := os.Create(strName)
+		if err != nil {
+			log.LOG_Error("Error", zap.Error(err))
+		}
+		defer f.Close()
+		for partIdx, buff := range fileBuff {
+			position := partIdx * domain.FilePayloadSize
+			_, err := f.WriteAt(buff, int64(position))
+			if err != nil {
+				log.LOG_Error("Error", zap.Error(err))
+			}
+		}
+		log.LOG_Info("File save Completed :", zap.String("fileName", f.Name()))
+	},
+}
+
 func init() {
 	File.AddCmd(Upload)
 	File.AddCmd(Download)
+	File.AddCmd(DownloadMultiConnection)
+
+}
+
+func downloadWorker(workerIdx int, wg *sync.WaitGroup, partQueue chan int, fileBuff map[int][]byte, fileLock *sync.Mutex, x *msg.MediaDocument) {
+	defer wg.Done()
+
+	log.LOG_Info("Worker Started :", zap.Int("worker", workerIdx))
+	for {
+		select {
+		case partIdx := <-partQueue:
+			ctx := filemanager.Ctx()
+			req := new(msg.FileGet)
+			req.Location = &msg.InputFileLocation{
+				AccessHash: x.Doc.AccessHash,
+				ClusterID:  x.Doc.ClusterID,
+				FileID:     x.Doc.ID,
+				Version:    x.Doc.Version,
+			}
+			req.Offset = int32(partIdx * domain.FilePayloadSize)
+			req.Limit = domain.FilePayloadSize
+			if req.Offset+domain.FilePayloadSize > x.Doc.FileSize {
+				req.Limit = x.Doc.FileSize - (req.Offset)
+			}
+
+			reqBuff, _ := req.Marshal()
+
+			envelop := new(msg.MessageEnvelope)
+			envelop.Constructor = msg.C_FileGet
+			envelop.Message = reqBuff
+			envelop.RequestID = uint64(domain.SequentialUniqueID())
+
+			// Send
+			for _SDK.GetNetworkStatus() == int32(domain.DISCONNECTED) || _SDK.GetNetworkStatus() == int32(domain.CONNECTING) {
+				log.LOG_Warn("network is not connected", zap.Int("worker", workerIdx), zap.Int("PartIdx", partIdx))
+				time.Sleep(500 * time.Millisecond)
+			}
+
+			log.LOG_Debug("send download request", zap.Int("worker", workerIdx), zap.Int("PartIdx", partIdx))
+			res, err := ctx.Send(envelop)
+
+			if err == nil {
+				switch res.Constructor {
+				case msg.C_Error:
+					x := new(msg.Error)
+					x.Unmarshal(res.Message)
+					log.LOG_Error("received Error response", zap.Int("worker", workerIdx), zap.Int("PartIdx", partIdx), zap.String("Code", x.Code), zap.String("Item", x.Items))
+					// on error add to queue again
+					partQueue <- partIdx
+				case msg.C_File:
+					x := new(msg.File)
+					err := x.Unmarshal(res.Message)
+					if err != nil {
+						// on error add to queue again
+						partQueue <- partIdx
+						log.LOG_Error("failed to unmarshal C_File", zap.Int("worker", workerIdx), zap.Int("PartIdx", partIdx), zap.Error(err))
+					} else {
+						fileLock.Lock()
+						fileBuff[partIdx] = x.Bytes
+						fileLock.Unlock()
+					}
+				default:
+					// on error add to queue again
+					partQueue <- partIdx
+					log.LOG_Error("received unknown response", zap.Int("worker", workerIdx), zap.Int("PartIdx", partIdx), zap.Error(err))
+				}
+			} else {
+				// on error add to queue again
+				partQueue <- partIdx
+				log.LOG_Error("downloadWorker()", zap.Int("worker", workerIdx), zap.Int("PartIdx", partIdx), zap.Error(err))
+			}
+
+		default:
+			log.LOG_Info("Worker Exited :", zap.Int("worker", workerIdx))
+			return
+		}
+	}
 
 }
