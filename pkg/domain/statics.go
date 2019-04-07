d14774180f0ecf84249f748693f64b7d1ffea802
Ehsan N. Moosa <ehsan.code@nested.me>
2019-04-07 11:14:32 +0430

    spell checking

--- pkg/domain/statics.go
+++ pkg/domain/statics.go
@@ -48,26 +48,26 @@ func init() {
 // SplitPQ ...
 // This function used for proof of work to splits PQ to two prime numbers P and Q
 func SplitPQ(pq *big.Int) (p1, p2 *big.Int) {
-	value_0 := big.NewInt(0)
-	value_1 := big.NewInt(1)
-	value_15 := big.NewInt(15)
-	value_17 := big.NewInt(17)
-	rndmax := big.NewInt(0).SetBit(big.NewInt(0), 64, 1)
+	value0 := big.NewInt(0)
+	value1 := big.NewInt(1)
+	value15 := big.NewInt(15)
+	value17 := big.NewInt(17)
+	rndMax := big.NewInt(0).SetBit(big.NewInt(0), 64, 1)
 
 	what := big.NewInt(0).Set(pq)
 	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 	g := big.NewInt(0)
 	i := 0
-	for !(g.Cmp(value_1) == 1 && g.Cmp(what) == -1) {
-		q := big.NewInt(0).Rand(rnd, rndmax)
-		q = q.And(q, value_15)
-		q = q.Add(q, value_17)
+	for !(g.Cmp(value1) == 1 && g.Cmp(what) == -1) {
+		q := big.NewInt(0).Rand(rnd, rndMax)
+		q = q.And(q, value15)
+		q = q.Add(q, value17)
 		q = q.Mod(q, what)
 
-		x := big.NewInt(0).Rand(rnd, rndmax)
-		whatnext := big.NewInt(0).Sub(what, value_1)
-		x = x.Mod(x, whatnext)
-		x = x.Add(x, value_1)
+		x := big.NewInt(0).Rand(rnd, rndMax)
+		whatNext := big.NewInt(0).Sub(what, value1)
+		x = x.Mod(x, whatNext)
+		x = x.Add(x, value1)
 
 		y := big.NewInt(0).Set(x)
 		lim := 1 << (uint(i) + 18)
@@ -79,9 +79,9 @@ func SplitPQ(pq *big.Int) (p1, p2 *big.Int) {
 			b := big.NewInt(0).Set(x)
 			c := big.NewInt(0).Set(q)
 
-			for b.Cmp(value_0) == 1 {
+			for b.Cmp(value0) == 1 {
 				b2 := big.NewInt(0)
-				if b2.And(b, value_1).Cmp(value_0) == 1 {
+				if b2.And(b, value1).Cmp(value0) == 1 {
 					c.Add(c, a)
 					if c.Cmp(what) >= 0 {
 						c.Sub(c, what)
@@ -109,7 +109,7 @@ func SplitPQ(pq *big.Int) (p1, p2 *big.Int) {
 			}
 			j = j + 1
 
-			if g.Cmp(value_1) != 0 {
+			if g.Cmp(value1) != 0 {
 				flag = false
 			}
 		}
