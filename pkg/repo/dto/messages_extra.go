3b97647cd80e207dec4ce6176065f3888346fd28
Behrooz Ahadian <ahadian.behroz@gmail.com>
2019-04-09 12:37:20 +0430

    set primary key for message extra

--- pkg/repo/dto/messages_extra.go
+++ pkg/repo/dto/messages_extra.go
@@ -2,8 +2,8 @@ package dto
 
 type MessagesExtra struct {
 	dto
-	PeerID    int64 `gorm:"column:PeerID" json:"PeerID"`
-	PeerType  int32 `gorm:"column:PeerType" json:"PeerType"`
+	PeerID    int64 `gorm:"type:bigint;primary_key;column:PeerID" json:"PeerID"`
+	PeerType  int32 `gorm:"type:integer;primary_key;column:PeerType" json:"PeerType"`
 	ScrollID int64 `gorm:"column:ScrollID" json:"ScrollID"`
 }
 
