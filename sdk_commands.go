c8970dced4e869751a56215c7f27946bfbbbca70
Ehsan N. Moosa <ehsan.code@nested.me>
2019-05-08 00:07:10 +0430

    fix message hole

--- sdk_commands.go
+++ sdk_commands.go
@@ -289,7 +289,20 @@ func (r *River) messagesGetHistory(in, out *msg.MessageEnvelope, timeoutCB domai
 		}
 		messages, users := repo.Messages.GetMessageHistory(req.Peer.ID, int32(req.Peer.Type), bar.Min, bar.Max, req.Limit)
 		if len(messages) < int(req.Limit) {
-			r.queueCtrl.ExecuteCommand(in.RequestID, in.Constructor, in.Message, timeoutCB, successCB, true)
+			cb := func(m *msg.MessageEnvelope) {
+				switch m.Constructor {
+				case msg.C_MessagesMany:
+					x := new(msg.MessagesMany)
+					_ = x.Unmarshal(m.Message)
+					if len(x.Messages) < int(req.Limit) {
+						_ = messageHole.SetLowerFilled(req.Peer.ID, int32(req.Peer.Type))
+					}
+				case msg.C_Error:
+				default:
+				}
+				successCB(m)
+			}
+			r.queueCtrl.ExecuteCommand(in.RequestID, in.Constructor, in.Message, timeoutCB, cb, true)
 			return
 		}
 		messagesGetHistory(out, messages, users, in.RequestID, successCB)
