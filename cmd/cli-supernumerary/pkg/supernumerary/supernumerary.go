65f2bac5e2b35ce0c9b3684e367aa9de9a088d6e
Ehsan N. Moosa <ehsan.code@nested.me>
2019-06-21 09:54:52 +0430

    supernumerary client fixed

--- cmd/cli-supernumerary/pkg/supernumerary/supernumerary.go
+++ cmd/cli-supernumerary/pkg/supernumerary/supernumerary.go
@@ -123,6 +123,7 @@ func (s *Supernumerary) CreateAuthKey() {
 	waitGroup := sync.WaitGroup{}
 	waitGroup.Add(len(s.Actors))
 	_Log.Info("Creating DH Keys ...")
+	scenario.LoadServerKeys()
 	scenario.GenDhPrivateKey()
 	_Log.Info("Dh Keys Generated ...")
 	for _, act := range s.Actors {
