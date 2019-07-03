1293a310777563aa50073e3f1e14f733b2fc05db
Reza pilehvar <rezapilehvar@Rezas-Mac.local>
2019-07-03 12:21:32 +0430

    add realtopmessage function

--- sdk_cmd_db.go
+++ sdk_cmd_db.go
@@ -1,9 +1,23 @@
 package riversdk
 
-import "git.ronaksoftware.com/ronak/riversdk/pkg/repo"
+import (
+	"git.ronaksoftware.com/ronak/riversdk/pkg/repo"
+	"git.ronaksoftware.com/ronak/riversdk/pkg/logs"
+	"go.uber.org/zap"
+)
 
 func (r *River) IsMessageExist(messageID int64) bool {
 	message := repo.Messages.GetMessage(messageID)
 
 	return message != nil
+}
+
+// GetRealTopMessageID returns max message id
+func (r *River) GetRealTopMessageID(peerID int64, peerType int32) int64 {
+	topMsgID, err := repo.Messages.GetTopMessageID(peerID, peerType)
+	if err != nil {
+		logs.Error("SDK::GetRealTopMessageID() => Messages.GetTopMessageID()", zap.Error(err))
+		return -1
+	}
+	return topMsgID
 }
