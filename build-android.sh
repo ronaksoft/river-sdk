56bb8926bad6e44229258f79822e44aef1cf8a90
Reza Pilehvar <graphreza@gmail.com>
2019-07-09 20:05:41 +0430

    add String func in messageHole

--- build-android.sh
+++ build-android.sh
@@ -1,4 +1,3 @@
 ##!/usr/bin/env bash
 
-export ANDROID_HOME="/users/rezapilehvar/Library/Android/sdk"
-gomobile bind -target=android -o=/users/rezapilehvar/desktop/RiverAndroid/mainmodule/sdk/riversdk.aar git.ronaksoftware.com/ronak/riversdk
+gomobile bind -target=android -o=$RIVER_ANDROID_PATH/mainmodule/sdk/riversdk.aar git.ronaksoftware.com/ronak/riversdk
