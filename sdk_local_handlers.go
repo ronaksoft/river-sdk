9b53ac520b176069c47f3cbc71f3c84d63934085
Ehsan N. Moosa <ehsan.code@nested.me>
2019-09-14 17:37:10 +0430

    fix pending in get history

--- sdk_local_handlers.go
+++ sdk_local_handlers.go
@@ -232,7 +232,7 @@ func (r *River) messagesGetHistory(in, out *msg.MessageEnvelope, timeoutCB domai
 	// Prepare the the result before sending back to the client
 	pendingMessages := repo.PendingMessages.GetByPeer(req.Peer.ID, int32(req.Peer.Type))
 	preSuccessCB := func(cb domain.MessageHandler, pms []*msg.UserMessage, req *msg.MessagesGetHistory) domain.MessageHandler {
-		topMessageID := dtoDialog.TopMessageID
+		topMsgID, _ := repo.Messages.GetTopMessageID(req.Peer.ID, int32(req.Peer.Type))
 		return func(m *msg.MessageEnvelope) {
 			switch m.Constructor {
 			case msg.C_MessagesMany:
@@ -258,7 +258,7 @@ func (r *River) messagesGetHistory(in, out *msg.MessageEnvelope, timeoutCB domai
 				if len(pms) > 0 {
 					// 2nd base on the reqMin values add the appropriate pending messages
 					switch {
-					case req.MaxID >= topMessageID:
+					case topMsgID > 0 && req.MaxID >= topMsgID:
 						x.Messages = append(x.Messages, pms...)
 					default:
 						// Min != 0
