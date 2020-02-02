49bfbe3728fbaafc26e228ac610e20ac77576e0d
Ehsan N. Moosa <ehsan.code@nested.me>
2020-02-02 12:24:48 +0330

    fix Photo Gallery sort

--- sdk_local_handlers.go
+++ sdk_local_handlers.go
@@ -1110,6 +1110,9 @@ func (r *River) usersGetFull(in, out *msg.MessageEnvelope, timeoutCB domain.Time
 		res := new(msg.UsersMany)
 		for _, user := range users {
 			user.PhotoGallery = repo.Users.GetPhotoGallery(user.ID)
+			sort.Slice(user.PhotoGallery, func(i, j int) bool {
+				return user.PhotoGallery[i].PhotoID < user.PhotoGallery[j].PhotoID
+			})
 		}
 		res.Users = users
 
