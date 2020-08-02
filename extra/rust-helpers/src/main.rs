2b02351f2ab6623264665ce20e3948901d342f4c
Ehsan N. Moosa <ehsan.code@nested.me>
2020-08-02 11:56:25 +0430

    remove RUST codes

--- extra/rust-helpers/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![allow(dead_code, unused)]
-use std::vec;
-
-fn main() {
-    let a :[u8; 100] = [0; 100];
-    let mut v : Vec<u8> = Vec::new();
-    v.extend_from_slice(a.iter().as_slice());
-    println!("{:?}", v);
-}
