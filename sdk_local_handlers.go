181b86d29745662af8a860366a2a07a1f51d24d7
Reza Pilehvar <graphreza@gmail.com>
2020-08-31 17:54:57 +0430

    change getCurrentTeam to inputMessageEnvlope.Team

--- sdk_local_handlers.go
+++ sdk_local_handlers.go
@@ -201,7 +201,7 @@ func (r *River) messagesSend(in, out *msg.MessageEnvelope, timeoutCB domain.Time
 	// this will be used as next requestID
 	req.RandomID = domain.SequentialUniqueID()
 	msgID := -req.RandomID
-	res, err := repo.PendingMessages.Save(GetCurrTeam(), msgID, r.ConnInfo.UserID, req)
+	res, err := repo.PendingMessages.Save(in.Team, msgID, r.ConnInfo.UserID, req)
 	if err != nil {
 		e := new(msg.Error)
 		e.Code = "n/a"
@@ -512,7 +512,7 @@ func (r *River) messagesSendMedia(in, out *msg.MessageEnvelope, timeoutCB domain
 		// Insert into pending messages, id is negative nano timestamp and save RandomID too : Done
 		dbID := -req.RandomID
 
-		res, err := repo.PendingMessages.SaveMessageMedia(GetCurrTeam(), dbID, r.ConnInfo.UserID, req)
+		res, err := repo.PendingMessages.SaveMessageMedia(in.Team, dbID, r.ConnInfo.UserID, req)
 		if err != nil {
 			e := &msg.Error{}
 			e.Code = "n/a"
@@ -536,7 +536,7 @@ func (r *River) messagesSendMedia(in, out *msg.MessageEnvelope, timeoutCB domain
 			Constructor: msg.C_MessagesSendMedia,
 			RequestID:   uint64(req.RandomID),
 			Message:     requestBytes,
-			Team:        GetCurrTeam(),
+			Team:        in.Team,
 		},
 		timeoutCB, successCB, true,
 	)
@@ -1493,7 +1493,7 @@ func (r *River) clientSendMessageMedia(in, out *msg.MessageEnvelope, timeoutCB d
 	}
 
 	h, _ := domain.CalculateSha256(reqMedia.FilePath)
-	pendingMessage, err := repo.PendingMessages.SaveClientMessageMedia(GetCurrTeam(), msgID, r.ConnInfo.UserID, fileID, fileID, thumbID, reqMedia, h)
+	pendingMessage, err := repo.PendingMessages.SaveClientMessageMedia(in.Team, msgID, r.ConnInfo.UserID, fileID, fileID, thumbID, reqMedia, h)
 	if err != nil {
 		e := new(msg.Error)
 		e.Code = "n/a"
