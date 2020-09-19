b71c78f1abe0284c5c3d6dea90e757400c1ec467
Ehsan N. Moosa <ehsan.code@nested.me>
2020-09-19 23:17:19 +0430

    cleaning

--- pkg/repo/dto/messages_extra.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type MessagesExtra struct {
-	dto
-	PeerID   int64  `json:"PeerID"`
-	PeerType int32  `json:"PeerType"`
-	ScrollID int64  `json:"ScrollID"`
-	Holes    []byte `json:"Holes"`
-}
--- pkg/repo/repo_messages_extra.go
+++ pkg/repo/repo_messages_extra.go
@@ -4,13 +4,19 @@ import (
 	"encoding/json"
 	"git.ronaksoft.com/river/sdk/internal/pools"
 	"git.ronaksoft.com/river/sdk/internal/tools"
-	"git.ronaksoft.com/river/sdk/pkg/repo/dto"
 	"github.com/dgraph-io/badger/v2"
 )
 
 const (
 	prefixMessageExtra = "MSG_EX"
 )
+
+type MessagesExtraItem struct {
+	PeerID   int64  `json:"PeerID"`
+	PeerType int32  `json:"PeerType"`
+	ScrollID int64  `json:"ScrollID"`
+	Holes    []byte `json:"Holes"`
+}
 
 type repoMessagesExtra struct {
 	*repository
@@ -28,8 +34,8 @@ func (r *repoMessagesExtra) getKey(teamID, peerID int64, peerType int32) []byte 
 	return id
 }
 
-func (r *repoMessagesExtra) get(teamID, peerID int64, peerType int32) *dto.MessagesExtra {
-	message := new(dto.MessagesExtra)
+func (r *repoMessagesExtra) get(teamID, peerID int64, peerType int32) *MessagesExtraItem {
+	message := &MessagesExtraItem{}
 	_ = badgerView(func(txn *badger.Txn) error {
 		item, err := txn.Get(r.getKey(teamID, peerID, peerType))
 		if err != nil {
@@ -42,7 +48,7 @@ func (r *repoMessagesExtra) get(teamID, peerID int64, peerType int32) *dto.Messa
 	return message
 }
 
-func (r *repoMessagesExtra) save(key []byte, m *dto.MessagesExtra) {
+func (r *repoMessagesExtra) save(key []byte, m *MessagesExtraItem) {
 	bytes, _ := json.Marshal(m)
 	_ = badgerUpdate(func(txn *badger.Txn) error {
 		return txn.SetEntry(badger.NewEntry(key, bytes))
