e64b0ad0941dcff27c994910c7ea76d53dd2be28
Reza Pilehvar <graphreza@gmail.com>
2020-11-07 12:08:06 +0330

    chanage river module path

--- build-android-prime.sh
+++ build-android-prime.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gomobile bind -target=android -ldflags="-s -w" -o=$RIVER_ANDROID_PATH/mainmodule/sdk/riversdk.aar git.ronaksoft.com/river/sdk/sdk/prime
+gomobile bind -target=android -ldflags="-s -w" -o=$RIVER_ANDROID_PATH/river-module/sdk/riversdk.aar git.ronaksoft.com/river/sdk/sdk/prime
