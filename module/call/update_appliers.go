0a5c315ce9176924cd03004b436a2eacba7882c5
Hamidreza KK <hamidrezakks@gmail.com>
2021-05-16 10:33:17 +0430

    fix

--- module/call/update_appliers.go
+++ module/call/update_appliers.go
@@ -9,66 +9,71 @@ import (
 
 func (c *call) updatePhoneCall(u *msg.UpdateEnvelope) (res []*msg.UpdateEnvelope, err error) {
 	//go func() {
-		if u.Constructor != msg.C_UpdatePhoneCall {
-			return
-		}
+	if u.Constructor != msg.C_UpdatePhoneCall {
+		return
+	}
 
-		x := &msg.UpdatePhoneCall{}
-		err = x.Unmarshal(u.Update)
-		if err != nil {
-			return
-		}
+	x := &msg.UpdatePhoneCall{}
+	err = x.Unmarshal(u.Update)
+	if err != nil {
+		return
+	}
 
-		logs.Debug("updatePhoneCall", zap.Int32("action", int32(x.Action)))
+	logs.Debug("updatePhoneCall", zap.Int32("action", int32(x.Action)))
 
-		now := domain.Now().Unix()
-		if !(x.Timestamp == 0 || now-x.Timestamp < 60) {
-			return
-		}
+	now := domain.Now().Unix()
+	if !(x.Timestamp == 0 || now-x.Timestamp < 60) {
+		return
+	}
 
-		data, err := parseCallAction(x.Action, x.ActionData)
-		if err != nil {
-			logs.Debug("parseCallAction", zap.Error(err))
-			return
-		}
+	data, err := parseCallAction(x.Action, x.ActionData)
+	if err != nil {
+		logs.Debug("parseCallAction", zap.Error(err))
+		return
+	}
 
-		update := &UpdatePhoneCall{
-			UpdatePhoneCall: x,
-			Data:            data,
-		}
+	update := &UpdatePhoneCall{
+		UpdatePhoneCall: x,
+		Data:            data,
+	}
 
-		switch x.Action {
-		case msg.PhoneCallAction_PhoneCallRequested:
-			c.callRequested(update)
-		case msg.PhoneCallAction_PhoneCallAccepted:
-			c.callAccepted(update)
-		case msg.PhoneCallAction_PhoneCallDiscarded:
-			c.callDiscarded(update)
-		case msg.PhoneCallAction_PhoneCallIceExchange:
-			c.iceExchange(update)
-		case msg.PhoneCallAction_PhoneCallMediaSettingsChanged:
-			c.mediaSettingsUpdated(update)
-		case msg.PhoneCallAction_PhoneCallSDPOffer:
-			c.sdpOfferUpdated(update)
-		case msg.PhoneCallAction_PhoneCallSDPAnswer:
-			c.sdpAnswerUpdated(update)
-		case msg.PhoneCallAction_PhoneCallAck:
-			c.callAcknowledged(update)
-		case msg.PhoneCallAction_PhoneCallParticipantAdded:
-			c.participantAdded(update)
-		case msg.PhoneCallAction_PhoneCallParticipantRemoved:
-			c.participantRemoved(update)
-		case msg.PhoneCallAction_PhoneCallAdminUpdated:
-			c.adminUpdated(update)
-		case msg.PhoneCallAction_PhoneCallJoinRequested:
-			c.joinRequested(update)
-		case msg.PhoneCallAction_PhoneCallScreenShare:
-			c.screenShareUpdated(update)
-		case msg.PhoneCallAction_PhoneCallPicked:
-			c.callPicked(update)
-		case msg.PhoneCallAction_PhoneCallRestarted:
-			c.callRestarted(update)
-		}
+	logs.Info("update", zap.Any("action", x.Action), zap.Any("data", data))
+	if data == nil {
+		return
+	}
+
+	switch x.Action {
+	case msg.PhoneCallAction_PhoneCallRequested:
+		c.callRequested(update)
+	case msg.PhoneCallAction_PhoneCallAccepted:
+		c.callAccepted(update)
+	case msg.PhoneCallAction_PhoneCallDiscarded:
+		c.callDiscarded(update)
+	case msg.PhoneCallAction_PhoneCallIceExchange:
+		c.iceExchange(update)
+	case msg.PhoneCallAction_PhoneCallMediaSettingsChanged:
+		c.mediaSettingsUpdated(update)
+	case msg.PhoneCallAction_PhoneCallSDPOffer:
+		c.sdpOfferUpdated(update)
+	case msg.PhoneCallAction_PhoneCallSDPAnswer:
+		c.sdpAnswerUpdated(update)
+	case msg.PhoneCallAction_PhoneCallAck:
+		c.callAcknowledged(update)
+	case msg.PhoneCallAction_PhoneCallParticipantAdded:
+		c.participantAdded(update)
+	case msg.PhoneCallAction_PhoneCallParticipantRemoved:
+		c.participantRemoved(update)
+	case msg.PhoneCallAction_PhoneCallAdminUpdated:
+		c.adminUpdated(update)
+	case msg.PhoneCallAction_PhoneCallJoinRequested:
+		c.joinRequested(update)
+	case msg.PhoneCallAction_PhoneCallScreenShare:
+		c.screenShareUpdated(update)
+	case msg.PhoneCallAction_PhoneCallPicked:
+		c.callPicked(update)
+	case msg.PhoneCallAction_PhoneCallRestarted:
+		c.callRestarted(update)
+	}
 	//}()
 
 	res = []*msg.UpdateEnvelope{u}
--- module/call/utils.go
+++ module/call/utils.go
@@ -64,112 +64,112 @@ func parseCallAction(constructor msg.PhoneCallAction, data []byte) (out interfac
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t1, nil
 	case msg.PhoneCallAction_PhoneCallAccepted:
 		t2 := &msg.PhoneActionAccepted{}
 		err = t2.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t2, nil
 	case msg.PhoneCallAction_PhoneCallDiscarded:
 		t3 := &msg.PhoneActionDiscarded{}
 		err = t3.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t3, nil
 	case msg.PhoneCallAction_PhoneCallCallWaiting:
 		t4 := &msg.PhoneActionCallWaiting{}
 		err = t4.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t4, nil
 	case msg.PhoneCallAction_PhoneCallIceExchange:
 		t5 := &msg.PhoneActionIceExchange{}
 		err = t5.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t5, nil
 	case msg.PhoneCallAction_PhoneCallEmpty:
 		t6 := &msg.PhoneActionCallEmpty{}
 		err = t6.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t6, nil
 	case msg.PhoneCallAction_PhoneCallMediaSettingsChanged:
 		t7 := &msg.PhoneActionMediaSettingsUpdated{}
 		err = t7.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t7, nil
 	case msg.PhoneCallAction_PhoneCallSDPOffer:
 		t8 := &msg.PhoneActionSDPOffer{}
 		err = t8.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t8, nil
 	case msg.PhoneCallAction_PhoneCallSDPAnswer:
 		t9 := &msg.PhoneActionSDPAnswer{}
 		err = t9.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t9, nil
 	case msg.PhoneCallAction_PhoneCallParticipantAdded:
 		t10 := &msg.PhoneActionParticipantAdded{}
 		err = t10.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t10, nil
 	case msg.PhoneCallAction_PhoneCallParticipantRemoved:
 		t11 := &msg.PhoneActionParticipantRemoved{}
 		err = t11.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t11, nil
 	case msg.PhoneCallAction_PhoneCallJoinRequested:
 		t12 := &msg.PhoneActionJoinRequested{}
 		err = t12.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t12, nil
 	case msg.PhoneCallAction_PhoneCallAdminUpdated:
 		t13 := &msg.PhoneActionAdminUpdated{}
 		err = t13.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t13, nil
 	case msg.PhoneCallAction_PhoneCallScreenShare:
 		t14 := &msg.PhoneActionScreenShare{}
 		err = t14.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t14, nil
 	case msg.PhoneCallAction_PhoneCallPicked:
 		t15 := &msg.PhoneActionPicked{}
 		err = t15.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t15, nil
 	case msg.PhoneCallAction_PhoneCallRestarted:
 		t15 := &msg.PhoneActionRestarted{}
 		err = t15.Unmarshal(data)
 		if err != nil {
 			return
 		}
-		return out, nil
+		return t15, nil
 	}
 	return nil, ErrActionNotFound
 }
